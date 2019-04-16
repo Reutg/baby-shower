379eb34f741b9652d60d658d4e55ff592be83811
Benazuz <azuz419@gmail.com>
2019-04-16 14:36:25 +0300

    babyname search feature done

--- server/routes/api.js
+++ server/routes/api.js
@@ -16,45 +16,82 @@ router.get('/', function (req, res) {
     res.end()
 })
 
-router.get('/babyNames/:firstLetter/:popularity', function (req, res) {
+router.get('/babyNames/:firstLetter/:popularity/:gender', function (req, res) {
     let firstLetter = req.params.firstLetter
     firstLetter.toLowerCase()
     let popularity = req.params.popularity
-    // console.log(firstLetter)
+    let gender = req.params.gender
+
+    if(gender === "female"){
+        request(`https://data.cityofnewyork.us/resource/25th-nujf.json/`, function (err, response) {
+            let babyNamesNotFiltered = JSON.parse(response.body)
+            let babyNames = babyNamesNotFiltered.map(b => { return { name: b.nm, gender: b.gndr, popularity: b.rnk } })
+                .filter(b => b.name.slice(0, 1).toLowerCase() == firstLetter)
+                .filter((obj, pos, arr) => {
+                    return arr.map(mapObj =>
+                        mapObj.name).indexOf(obj.name) == pos;
+                });
     
-    request(`https://data.cityofnewyork.us/resource/25th-nujf.json/`, function (err, response) {
-        let babyNamesNotFiltered = JSON.parse(response.body)
-        let babyNames = babyNamesNotFiltered.map(b => { return { name: b.nm, gender: b.gndr, popularity: b.rnk } })
-            .filter(b => b.name.slice(0, 1).toLowerCase() == firstLetter)
-            .filter((obj, pos, arr) => {
-                return arr.map(mapObj =>
-                    mapObj.name).indexOf(obj.name) == pos;
-            });
+    
+            //low ranked names:
+            popularNames = []
+            unPopularNames = []
+            if (popularity === 'popular') {
+                let names = babyNames.filter(b => b.popularity < 45)
+                popularNames.push(names)
+                res.send(popularNames)
+            }
+            else if(popularity === 'unpopular') {
+                let names = babyNames.filter(b => b.popularity > 45)
+                unPopularNames.push(names)
+                res.send(unPopularNames)
+            }
+            else{
+                console.log('no popularity defined')
+                res.end()
+            }
+    
+            // res.send(babyNames)
+        })
+    }
 
+    else if(gender === "male"){
+        request(`https://data.cityofnewyork.us/resource/25th-nujf.json?gndr=MALE`, function (err, response) {
+            let babyNamesNotFiltered = JSON.parse(response.body)
+            let babyNames = babyNamesNotFiltered.map(b => { return { name: b.nm, gender: b.gndr, popularity: b.rnk } })
+                .filter(b => b.name.slice(0, 1).toLowerCase() == firstLetter)
+                .filter((obj, pos, arr) => {
+                    return arr.map(mapObj =>
+                        mapObj.name).indexOf(obj.name) == pos;
+                });
+    
+    
+            //low ranked names:
+            popularNames = []
+            unPopularNames = []
+            if (popularity === 'popular') {
+                let names = babyNames.filter(b => b.popularity < 45)
+                popularNames.push(names)
+                res.send(popularNames)
+            }
+            else if(popularity === 'unpopular') {
+                let names = babyNames.filter(b => b.popularity > 45)
+                unPopularNames.push(names)
+                res.send(unPopularNames)
+            }
+            else{
+                console.log('no popularity defined')
+                res.end()
+            }
+    
+            // res.send(babyNames)
+        })
+    }
+    else{
+        console.log('error - no gender specified')
+        res.end()
+    }
 
-        //low ranked names:
-        popularNames = []
-        unPopularNames = []
-        if (popularity === 'popular') {
-            let names = babyNames.filter(b => b.popularity < 45)
-            popularNames.push(names)
-            res.send(popularNames)
-        }
-        else if(popularity === 'unpopular') {
-            let names = babyNames.filter(b => b.popularity > 45)
-            unPopularNames.push(names)
-            res.send(unPopularNames)
-        }
-        else{
-            console.log('no popularity defined')
-            res.end()
-        }
-
-
-        // console.log('')
-        // console.log(foods)
-        // res.send(babyNames)
-    })
 
 })
 
